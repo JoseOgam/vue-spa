77222aabb2db112c1deff5631a8257312a8e7df3
JoseOgam <jotienoochieng@gmail.com>
2020-01-13 12:09:48 +0300

    vue pages build up

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,14 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+/**vue router section */
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const routes = [
+    {path: '/home', component: require('./components/Home.vue').default},
+];
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
